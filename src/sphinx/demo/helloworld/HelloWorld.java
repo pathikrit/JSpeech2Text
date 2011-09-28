dc3581b6ce309276ab72ec6cc744f62fd3b6b646
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2011-09-28 11:31:37 -0700

    play around with robot

--- src/sphinx/demo/helloworld/HelloWorld.java
+++ src/sphinx/demo/helloworld/HelloWorld.java
@@ -5,9 +5,11 @@ import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 
+import java.awt.*;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException {
         ConfigurationManager cm = new ConfigurationManager(HelloWorld.class.getResource("helloworld.config.xml"));
 
         Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
@@ -20,15 +22,24 @@ public class HelloWorld {
             return;
         }
 
-        System.out.println("Say: (Good morning | Hello) ( Bhiksha | Evandro | Paul | Philip | Rita | Will )");
+        Robot r = new Robot();
+        final int DELTA = 50;
 
         while (true) {
-            System.out.println("Start speaking...");
+            Point p = MouseInfo.getPointerInfo().getLocation();
             Result result = recognizer.recognize();
             if (result != null) {
-                System.out.println("You said: " + result.getBestFinalResultNoFiller());
-            } else {
-                System.out.println("I can't hear what you said.");
+                // System.out.println("You said: " + result.getBestFinalResultNoFiller() + "[" + result.getBestPronunciationResult() + "]");
+                String text = result.getBestFinalResultNoFiller();
+                if (text.equals("up")) {
+                    r.mouseMove(p.x, p.y - DELTA);
+                } else if (text.equals("down")) {
+                    r.mouseMove(p.x, p.y + DELTA);
+                } else if (text.equals("left")) {
+                    r.mouseMove(p.x - DELTA, p.y);
+                } else if (text.equals("right")) {
+                    r.mouseMove(p.x + DELTA, p.y);
+                }
             }
         }
     }
